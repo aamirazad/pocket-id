96c81c8740c2d23a68247975b20c0fa1b320f44e
Aamir Azad <aamirmazad@gmail.com>
2025-12-17 18:39:44 -0500

    chore: only build arm and amd for docker

--- scripts/development/build-binaries.sh
+++ scripts/development/build-binaries.sh
@@ -51,28 +51,11 @@ if [ "$DOCKER_ONLY" = true ]; then
     build_platform "linux-amd64" "linux" "amd64" ""
     build_platform "linux-arm64" "linux" "arm64" ""
 else
-    echo "Building for all platforms..."
-    # linux builds
+    echo "Building..."
     build_platform "linux-amd64" "linux" "amd64" ""
-    build_platform "linux-386" "linux" "386" ""
+
     build_platform "linux-arm64" "linux" "arm64" ""
-    build_platform "linux-armv7" "linux" "arm" "7"
 
-    # macOS builds
-    build_platform "macos-x64" "darwin" "amd64" ""
-    build_platform "macos-arm64" "darwin" "arm64" ""
-
-    # Windows builds
-    build_platform "windows-x64" "windows" "amd64" ""
-    build_platform "windows-arm64" "windows" "arm64" ""
-
-    # FreeBSD builds
-    build_platform "freebsd-amd64" "freebsd" "amd64" ""
-    build_platform "freebsd-arm64" "freebsd" "arm64" ""
-
-    # OpenBSD builds
-    build_platform "openbsd-amd64" "openbsd" "amd64" ""
-    build_platform "openbsd-arm64" "openbsd" "arm64" ""
 fi
 
 echo "Compilation done"
