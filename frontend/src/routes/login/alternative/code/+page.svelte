c37eef877e555db29e9eaf516a696188aec310e2
Aamir Azad <aamirmazad@gmail.com>
2025-12-20 12:47:30 -0500

    fix: back button actually goes back to login page

--- frontend/src/routes/login/alternative/code/+page.svelte
+++ frontend/src/routes/login/alternative/code/+page.svelte
@@ -15,16 +15,8 @@
 	let code = $state(data.code ?? '');
 	let isLoading = $state(false);
 	let error: string | undefined = $state();
-	let backHref = $state('/login');
 
 	const userService = new UserService();
-
-	// If the previous page is a Pocket ID page, go back there instead of the generic alternative login page
-	afterNavigate((e) => {
-		if (e.from?.url.pathname) {
-			backHref = e.from.url.pathname + e.from.url.search;
-		}
-	});
 
 	async function authenticate() {
 		isLoading = true;
@@ -70,7 +62,7 @@
 	<form onsubmit={preventDefault(authenticate)} class="w-full max-w-[450px]">
 		<Input id="Code" class="mt-7" placeholder={m.code()} bind:value={code} type="text" />
 		<div class="mt-8 flex justify-between gap-2">
-			<Button variant="secondary" class="flex-1" href={backHref}>{m.go_back()}</Button>
+			<Button variant="secondary" class="flex-1" href="/login">{m.go_back()}</Button>
 			<Button class="flex-1" type="submit" {isLoading}>{m.submit()}</Button>
 		</div>
 	</form>
